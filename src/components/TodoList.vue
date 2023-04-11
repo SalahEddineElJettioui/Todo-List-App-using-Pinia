ff7ac39e30e9dd1c667055fdb482c09b69809e24
salah <eljettiouisalaheddine@gmail.com>
2023-04-11 23:07:46 +0000

    printing the data in the page

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,22 @@
 <template>
 
+<div v-for=" todo in todoList" :key="todo.id" class="item"> 
+    <div class="content">
+        <span>
+            {{ todo.item }}
+        </span>
+    </div>
+</div>
+
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia';
+import { useTodoListStore } from '../stores/TodoList';
+
+
+const store = useTodoListStore()
+
+const { todoList } = storeToRefs(store);
+
 </script>
